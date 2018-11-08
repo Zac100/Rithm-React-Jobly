36112b8fee24e0e3ed0e57c9ce549aac01236ee0
Lulzasaur <frost9192@gmail.com>
2018-11-08 11:09:40 -0800

    added authorization components

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,6 +8,7 @@ const createToken = require("../helpers/createToken");
 
 router.post("/login", async function(req, res, next) {
   try {
+    console.log(req.body)
     const user = await User.authenticate(req.body);
     const token = createToken(user);
     return res.json({ token });
